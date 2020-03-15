bce159e28e58eb1813cc3b060a9d1d5ad3920945
raphael <rtnanje@gmail.com>
2020-03-15 02:49:04 -0400

    add module docstring

--- easyasyncproxy/__init__.py
+++ easyasyncproxy/__init__.py
@@ -1,8 +1,15 @@
-from .proxy.manager import AsyncProxyManager
+"""
+ProxyApi: Class for easy async requests with a pool of proxies
+AsyncProxyManager: The pool handle for ProxyApi
+Proxy: A proxy data class with convenience functions
+ScraperApi: https://www.scraperapi.com/
+ScraperKey: A data class with convenience functions
+ScrapeStackApi: https://scrapestack.com/
+"""
+
 from .proxy.api import ProxyApi
+from .proxy.manager import AsyncProxyManager
 from .proxy.proxy import Proxy
-
-from .scrapestack.api import ScrapeStackApi
-
 from .scraperapi.api import ScraperApi
 from .scraperapi.scraperkey import ScraperKey
+from .scrapestack.api import ScrapeStackApi
