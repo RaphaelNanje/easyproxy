b89553206f38e2b8f8cfcb25a8e967f7b9c3dd79
raphael <rtnanje@gmail.com>
2020-03-26 18:31:15 -0400

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='easyasyncproxy',
-    version='0.2.0',
+    version='1.1.0',
     packages=['easyasyncproxy', 'easyasyncproxy.proxy',
               'easyasyncproxy.proxy.tests',
               'easyasyncproxy.scraperapi',
